d335c1af6f2fa0b31982b5f4e1d0aa9b26298360
andreapmp <162078117+andreapmp@users.noreply.github.com>
2024-06-18 01:08:06 +0000

    coments added

--- src/api/routes.py
+++ src/api/routes.py
@@ -111,4 +111,11 @@ def get_invoices():
 
 
 
-
+# Work on the front end:
+# 1. create 3 new pages:  /Signup, /Login, /Private
+# update layout. js as well
+# 2. Create the necessary inputs needed for signup.js and login.js
+# 3. Make sure that they are controlled inputs (useState)
+# 4. include useContext and Context for flux applications
+# 5. update flux.js to have token, message, invoices in the store
+# 6. update and test actions to be able to retrieve a token and save it in localStorage
